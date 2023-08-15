d0cb67335934f62e0864f2708f7f0fe07a7263ac
Quinn Graves <quinngraves@Quinns-MacBook-Pro.local>
2023-08-15 15:07:57 -0400

    Troubleshooting name change h1 App.js

--- client/components/App.js
+++ client/components/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Baldur's Gate 3 Build Creator </h1>
+        <h1>Baldur's Gate 3 Creator </h1>
       </header>
     </div>
   );
